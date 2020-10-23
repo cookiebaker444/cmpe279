77a01dcdde4aab3fe2559cd09d1924ad8294e7ef
cookiebaker444 <43713840+cookiebaker444@users.noreply.github.com>
2020-10-22 22:22:01 -0700

    Add files via upload

---
+++ Assignment2/child.c
@@ -0,0 +1,21 @@
+// Server side C/C++ program to demonstrate Socket programming
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <sys/types.h>
+#define PORT 8080
+#define BUF_SIZE 100
+int main(int argc, char const *argv[]) {
+	int new_socket = atoi(argv[0]);
+	char buffer[1024] = {0};
+	char * hello = "Hello from second server";
+	int valread = read( new_socket , buffer, 1024); 
+	printf("%s\n",buffer ); 
+	send(new_socket , hello , strlen(hello) , 0 ); 
+	printf("Hello message sent from child\n"); 
+	return 0;
+	
+}
---
+++ Assignment2/server.c
@@ -0,0 +1,105 @@
+// Server side C/C++ program to demonstrate Socket programming
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#define PORT 8080
+int main(int argc, char const *argv[])
+{
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[102] = {0};
+    char *hello = "Hello from server";
+
+    printf("execve=0x%p\n", execve);
+
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Forcefully attaching socket to the port 8080
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+                                                  &opt, sizeof(opt)))
+    {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( PORT );
+
+    // Forcefully attaching socket to the port 8080
+    if (bind(server_fd, (struct sockaddr *)&address,
+                                 sizeof(address))<0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0)
+    {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+                       (socklen_t*)&addrlen))<0)
+    {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+    //valread = read( new_socket , buffer, 1024);
+    //printf("%s\n",buffer );
+    //send(new_socket , hello , strlen(hello) , 0 );
+    //printf("Hello message sent\n");
+    pid_t child = fork();
+
+    if (child < 0) {                        
+        perror("Child process creation failed");
+        exit(EXIT_FAILURE);
+    }
+    else if (child == 0) {
+	
+        if(setuid(65534) < 0){           
+            perror("Privilege dropping failed");
+            exit(EXIT_FAILURE);
+        }
+	//setuid(65534);
+        printf("User ID after privilege drop %d\n",getuid());
+	printf("privilege dropped\n");
+	char* execbuff[2];
+        char fdbuff[30];
+        sprintf(fdbuff, "%d", new_socket);
+        execbuff[0] = "./child";
+        execbuff[1] = fdbuff;
+        execbuff[2] = NULL;
+        execvp(execbuff[0], execbuff);
+
+        printf("Child process is done\n");
+
+	//child process processing the data
+	//valread = read( new_socket , buffer, 1024); 
+	//printf("%s\n",buffer ); 
+	//send(new_socket , hello , strlen(hello) , 0 ); 
+	//printf("Hello message sent from child\n"); 
+    }
+   
+    else {  
+	      
+        int stat = 0;
+	//valread = read( new_socket , buffer, 1024); 
+	//printf("%s\n",buffer ); 
+	//send(new_socket , hello , strlen(hello) , 0 ); 
+	//printf("Hello message sent from parent\n"); 
+        while (wait(&stat) > 0);   
+        printf("parent finish\n");
+    }     
+    return 0;
+}
